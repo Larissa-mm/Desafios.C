6ed599e0e966cc2243a4c95b236f2ff776608634
Larissa-mm <martinslarissamoreira@gmail.com>
2025-06-16 20:58:27 -0300

    add nivel aventureiro

--- desafioXadrez/Xadrez.C
+++ desafioXadrez/Xadrez.C
@@ -36,7 +36,38 @@ int main(){
         contador++; // incremento
     }
     
+    //---Nivel Aventureiro----
     
+    printf("\n");//quebra de linha 
+    printf("---- Movimentos do Cavalo ----\n");
+    // Constantes para o movimento específico do Cavalo
+    const int PASSOS_PARA_BAIXO = 2;
+    const int PASSOS_PARA_ESQUERDA = 1; 
+
+    //variável para controlar o loop de fora.
+    //inicia com o valor 0.
+    int controleLoop = 0;
+    //o loop 'while' vai continuar rodando ENQUANTO a variável 'controle_do_loop' for 0.
+    while (controleLoop == 0) {
+        // Imprime uma mensagem de início.
+        printf("Iniciando movimento em L do Cavalo:\n");
+        // Loop 'for' para os passos para baixo.
+        // Vai repetir o número de vezes definido em 'PASSOS_PARA_BAIXO'.
+        for (int i = 0; i < PASSOS_PARA_BAIXO; i++) {
+            printf("Baixo\n");
+        }
+
+        //Loop 'for' para os passos para a esquerda.
+        for (int i = 0; i < PASSOS_PARA_ESQUERDA; i++) {
+            printf("Esquerda\n");
+        }
+
+        // Agora mudei o valor da variável de controle para 1.
+        // Isso vai fazer o loop 'while' parar de repetir.
+        controleLoop = 1; 
+
+    } // Fim do loop while.
+
 
 
 
